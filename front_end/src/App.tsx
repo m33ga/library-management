eb1b5a221f33075bfd5fdeaf9f96bd2453d8ba8e
MarioSousaIPB <a45288@alunos.ipb.pt>
2024-11-15 19:32:06 +0000

    Added routing for login

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/Homepage/Homepage";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <div>
       <Routes>
-        {/* Directly define the HomePage as the default route */}
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
