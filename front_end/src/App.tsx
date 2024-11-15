3698404465800ff4010dac5fb6cbe32746d91548
MarioSousaIPB <a45288@alunos.ipb.pt>
2024-11-15 20:19:41 +0000

    Added routing for register

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/Homepage/Homepage";
 import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
