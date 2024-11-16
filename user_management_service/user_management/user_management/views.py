e154c1f4983c9e8b7e62759d555c2ba4de803a74
MarioSousaIPB <a45288@alunos.ipb.pt>
2024-11-16 17:40:39 +0000

    Created login view

--- user_management_service/user_management/user_management/views.py
+++ user_management_service/user_management/user_management/views.py
@@ -6,9 +6,16 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 
+from django.shortcuts import get_object_or_404
+
 @api_view(['POST'])
 def login(request):
-    return Response({})
+    user = get_object_or_404(User, username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response("missing user", status=status.HTTP_404_NOT_FOUND)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(user)
+    return Response({'token': token.key, 'user': serializer.data})
 
 
 @api_view(['POST'])
