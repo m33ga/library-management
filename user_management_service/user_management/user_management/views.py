c42692e749d28f52d84de00193b5f24333fdaaab
MarioSousaIPB <a45288@alunos.ipb.pt>
2024-11-16 17:47:13 +0000

    Added test token for checking authentification of user

--- user_management_service/user_management/user_management/views.py
+++ user_management_service/user_management/user_management/views.py
@@ -1,12 +1,12 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
-from .serializers import UserSerializer
 from rest_framework import status
-from rest_framework.authtoken.models import Token
-from django.contrib.auth.models import User
-
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from .serializers import UserSerializer
 
 @api_view(['POST'])
 def login(request):
@@ -27,10 +27,12 @@ def signup(request):
         user.set_password(request.data['password'])
         user.save()
         token = Token.objects.create(user=user)
-        return Response({"token": token.key, "user": serializer.data})
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'token': token.key, 'user': serializer.data})
+    return Response(serializer.errors, status=status.HTTP_200_OK)
 
-    
+
 @api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def test_token(request):
-    return Response({})
+    return Response("passed for {}".format(request.user.email))
