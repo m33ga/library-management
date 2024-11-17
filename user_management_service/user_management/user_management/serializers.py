19831f516379464c0cc9fd8773f0b8a6b7515f1a
MarioSousaIPB <a45288@alunos.ipb.pt>
2024-11-17 15:54:40 +0000

    Change login to use email, make email unique and fix the password returning in response

--- user_management_service/user_management/user_management/serializers.py
+++ user_management_service/user_management/user_management/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
 
-class UserSerializer (serializers.ModelSerializer):
-    class Meta(object):
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all(), message="This email is already in use.")]
+    )
+    password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
         model = User
-        fields =  ['id', 'username', 'password', 'email']
+        fields = ['id', 'username', 'password', 'email']
