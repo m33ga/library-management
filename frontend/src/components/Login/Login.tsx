ac629a1c4608eb62f34c9e96deba2b163c638d05
Guilherme Quintero Lorenzi <a64378@alunos.ipb.pt>
2024-11-17 17:07:06 +0000

    add the basic login

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,6 +1,24 @@
-import { useState } from "react";
 import "./Login.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form className="login-form">
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit" className="login-button">Login</button>
+        <p>If you don't have an account <a href="register" >click here</a></p>
+      </form>
+    </div>
+  );
 }
