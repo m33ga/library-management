d883696ab8fdf8319711b484c5ff9001051eaf07
MarioSousaIPB <a45288@alunos.ipb.pt>
2024-11-20 23:26:20 +0000

    Created model for userProfile (First added user connection with institute)

--- user_management_service/user_management/user_management/serializers.py
+++ user_management_service/user_management/user_management/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User, Group
 from rest_framework.validators import UniqueValidator
-from member_auth.models import Institution
+from member_auth.models import Institution, UserProfile
 
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
@@ -9,17 +9,23 @@ class UserSerializer(serializers.ModelSerializer):
         validators=[UniqueValidator(queryset=User.objects.all(), message="This email is already in use.")]
     )
     password = serializers.CharField(write_only=True, required=True)
+    institution_id = serializers.IntegerField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'email']
+        fields = ['id', 'username', 'password', 'email', 'institution_id']
 
     def create(self, validated_data):
-        user = super().create(validated_data)
 
+        institution_id = validated_data.pop('institution_id')
+        user = super().create(validated_data)
         member_group = Group.objects.get(name='member')
         user.groups.add(member_group)
+        institution = Institution.objects.get(id=institution_id)
+        UserProfile.objects.create(user=user, institution=institution)
+
         return user
+
 
 class InstitutionSerializer(serializers.ModelSerializer):
     class Meta:
