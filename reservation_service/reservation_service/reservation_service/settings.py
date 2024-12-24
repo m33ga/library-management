31cee9a1f827d161332143166681156030f3d776
Mihai Gurduza <m320964@alunos.ipb.pt>
2024-12-24 15:10:23 +0000

    feat: add reservation models with status and user response choices

--- reservation_service/reservation_service/reservation_service/settings.py
+++ reservation_service/reservation_service/reservation_service/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'book_reservations',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
